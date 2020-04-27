883d91f1d4a1cd7483b6861af2072f8ddb6b372e
Samuel Lesser <ineedmoreplates@gmail.com>
2020-04-27 03:39:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
   
-planeteer_calls.find do |array| array == valid_calls
+planeteer_calls.find do |call| call == valid_calls[]
 end
 end
